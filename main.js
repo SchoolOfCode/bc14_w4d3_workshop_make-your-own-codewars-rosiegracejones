9ec110fa172ee44b1aa5ee1ae6a353a48ff0531b
rosiegracejones <rosiegracejones@gmail.com>
2023-04-06 21:58:26 +0100

    kata written & empty function set up

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 /* 
-👉 Write your kata here!
+Welcome to the breakfast cafe! 
+
+We're taking breakfast orders; it’s your job to work out how many eggs the kitchen will need for each order.
+
+* for scrambled eggs, you’ll need 2 eggs
+* for an omelette you’ll need 3 eggs
+* for a fried egg, you’ll need 1 egg
+* you shouldn't count any other breakfast items.
+
+For example, the following order should require 8 eggs:
+[”scrambled egg”, “bacon”, “fried egg”, “toast”, “scrambled egg”, “omelette”]
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+function eggCount(breakfastOrder) {
+  // your code here
+}
