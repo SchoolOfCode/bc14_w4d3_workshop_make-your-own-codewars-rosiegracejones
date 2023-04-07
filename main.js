64fec8b04501e693235025ddab297e0504ea34ca
rosiegracejones <rosiegracejones@gmail.com>
2023-04-07 10:17:47 +0100

    second more difficult kato added to js notes

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 /* 
-Welcome to the breakfast cafe! 
+Kato 1 - Easy
 
+Welcome to the breakfast cafe! 
 We're taking breakfast orders; it’s your job to work out how many eggs the kitchen will need for each order.
 
 * for scrambled eggs, you’ll need 2 eggs
 * for an omelette you’ll need 3 eggs
 * for a fried egg, you’ll need 1 egg
-* you shouldn't count any other breakfast items.
 * IMPORTANT: if an order contains over 25 eggs, you must return "Oh no, we've run out of eggs! Please order something else."
 
 For example, the following order should require 8 eggs:
@@ -16,7 +16,38 @@ For example, the following order should require 8 eggs:
 // Here's your starter code:
 
 export function eggCount(breakfastOrder) {
-    // your code here!
+  // your code here!
 }
 
+/* 
+Kato 2 - Less Easy!
 
+It's time for the breakfast cafe's customers to pay their bill! 
+You'll need to work out how much each customer owes for their order, with the following pricing:
+
+Mains
+* full english - £12.50 (main)
+* vegan breakfast - £11 (main)
+* eggs royale £9 (main)
+
+Drinks
+* coffee £3
+* tea £2
+
+Extras
+* pain au chocolat £2.50
+* toast £1.50
+
+* IMPORTANT: if 3 'main' meals or more are ordered, all hot drinks for the entire order are FREE.
+
+Example:
+
+([{main: "full english", drink: "coffee"}, {main: "full english", drink: "tea"}, {main: "vegan breakfast", extras: "toast"}])
+
+should return: ([12.50, 12.50, 12.50])
+
+*/
+
+export function splitBill(orderItems) {
+  // your code here!
+}
